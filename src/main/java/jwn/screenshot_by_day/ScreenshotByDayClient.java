0ad83efea4d6b62ae3d58124188140e8480f4663
JWN <naa3000@naver.com>
2025-03-27 21:26:39 +0900

    save screenshot by day

--- src/main/java/jwn/screenshot_by_day/ScreenshotByDayClient.java
+++ src/main/java/jwn/screenshot_by_day/ScreenshotByDayClient.java
@@ -7,17 +7,8 @@ import org.slf4j.LoggerFactory;
 public class ScreenshotByDayClient implements ClientModInitializer {
 	public static final String MOD_ID = "screenshot_by_day";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitializeClient() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/jwn/screenshot_by_day/mixin/ScreenshotRecorderMixin.java
@@ -0,0 +1,35 @@
+package jwn.screenshot_by_day.mixin;
+
+import net.minecraft.client.util.ScreenshotRecorder;
+import net.minecraft.util.Util;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Mixin(ScreenshotRecorder.class)
+public class ScreenshotRecorderMixin {
+	/**
+	 * @author JWN
+	 * @reason Store screenshots into yyyy-MM-dd folders
+	 */
+	@Overwrite
+	private static File getScreenshotFilename(File directory) {
+		String today = LocalDate.now().format(DateTimeFormatter.ISO_DATE); // 예: "2025-03-27"
+		File datedDir = new File(directory, today);
+		datedDir.mkdir();
+
+		String baseName = Util.getFormattedCurrentTime(); // "2025-03-27_12.34.56"
+		int i = 1;
+
+		while (true) {
+			File file = new File(datedDir, baseName + (i == 1 ? "" : "_" + i) + ".png");
+			if (!file.exists()) {
+				return file;
+			}
+			++i;
+		}
+	}
+}
