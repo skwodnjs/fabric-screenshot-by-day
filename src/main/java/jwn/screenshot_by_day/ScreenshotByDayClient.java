e13e14d992c595e94c1447d12c778620ed00f466
JWN <naa3000@naver.com>
2025-04-27 03:16:34 +0900

    edit 'gradle.properties' and 'fabric.mod.json' for 1.21.4

--- src/main/java/jwn/screenshot_by_day/ScreenshotByDayClient.java
+++ src/main/java/jwn/screenshot_by_day/ScreenshotByDayClient.java
@@ -1,8 +1,6 @@
 package jwn.screenshot_by_day;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ScreenshotByDayClient implements ClientModInitializer {
 	public static final String MOD_ID = "screenshot_by_day";
